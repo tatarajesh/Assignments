1eddb3472118737be84164bc5b14a2fcb29dc3d1
tatarajesh <tatarajesh97@gmail.com>
2023-02-27 14:59:05 -0600

    final commit

--- Assignment-1/Question-1/src/data_Analysis.R
+++ Assignment-1/Question-1/src/data_Analysis.R
@@ -1,1 +1,6 @@
-g
+data=read.csv("C:/Users/Tata Rajesh Chowdary/Desktop/PDS/Assignments/Assignment-1/Question-1/data_clean/clean_data.csv")
+View(data)
+
+plot(data$Height.Inches.,data$Weight.Pounds.,xlab="Height", ylab="Weight",main="Height Vs Weight")
+
+plot(data$Age,data$Grip.Strength,xlab="Age", ylab="Strength", main="Height Vs Weight")
